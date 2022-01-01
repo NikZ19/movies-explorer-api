627b6f1e75a4fdd9d7cda39de52961646fefc78c
NikZ <gizm55@yandex.ru>
2022-01-01 22:08:02 +0300

    add cors

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ import mongoose from 'mongoose';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import error from './middlewares/error';
 import { requestLogger, errorLogger } from './middlewares/logger';
 import routes from './routes/index';
 import limiter from './middlewares/rateLimit';
+import corsOptions from './utils/corsOptions';
 
 const { PORT = 3000, DB_PATH } = process.env;
 
 const app = express();
 
 mongoose.connect(DB_PATH);
+
+app.use(cors(corsOptions));
 
 app.use(helmet());
 app.use(bodyParser.json());
